79187a6eda9c33b7a1c6bb8055fd316201bc5d42
khaoulaelm <136265673+khaoulaelm@users.noreply.github.com>
2026-02-22 21:22:28 +0100

    Update 2. annotation.sh

--- 4. Large-scale genome dataset/CGCD approach/2. annotation.sh
+++ 4. Large-scale genome dataset/CGCD approach/2. annotation.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Annotation with Prokka
+
 mkdir -p prokka_results
 
-for file in ~/genomes/*.fasta; do
-    base=$(basename "$file" .fasta)
+for file in ~renamed/*.fna; do
+    base=$(basename "$file" .fna)
     prokka --outdir prokka_results/$base --prefix $base --cpus 4 "$file"
 done
