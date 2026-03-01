9ccf30fadf9f88fd0624b859660aa7fd5c4554ca
khaoulaelm <136265673+khaoulaelm@users.noreply.github.com>
2026-03-01 21:23:18 +0100

    Update annotation.sh

--- 2. Genomic analyses/2.1. Prokka/annotation.sh
+++ 2. Genomic analyses/2.1. Prokka/annotation.sh
@@ -2,7 +2,10 @@
 
 mkdir -p prokka_results
 
-for file in ~/Bacterial_species_delimitation/1. Bacterial strains/genomes/*.fasta; do
-    base=$(basename "$file" .fasta)
-    prokka --outdir prokka_results/$base --prefix $base --cpus 4 "$file"
+GENOMES_DIR="$HOME/Bacterial_species_delimitation/1. Bacterial strains/genomes"
+
+for file in "$GENOMES_DIR"/*.fasta; do
+    base="$(basename "$file" .fasta)"
+    prokka --outdir "prokka_results/$base" --prefix "$base" --cpus 4 "$file"
 done
+
