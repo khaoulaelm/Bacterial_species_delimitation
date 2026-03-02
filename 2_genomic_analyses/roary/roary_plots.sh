030e33301e799bc9d28996905ca99d08d3bf0a4b
khaoulaelm <136265673+khaoulaelm@users.noreply.github.com>
2026-03-02 14:09:27 +0100

    Update roary_plots.sh

--- 2_genomic_analyses/roary/roary_plots.sh
+++ 2_genomic_analyses/roary/roary_plots.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+# Author: Khaoula El Mchachti
+# Description: Plotting Roary results using roary_plots.py
+# Input: tree.newick, gene_presence_absence.csv
+# Output: Roary plot files (plots)
+# Date: 2026-03-02
 
-echo "Starting Roary plotting"
+echo "===== Starting Roary plotting ======"
 
 # Plot Roary results
 echo "Plotting Roary results..."
