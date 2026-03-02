ba376a945ee499c91362f2d21d944defa1e071e1
khaoulaelm <136265673+khaoulaelm@users.noreply.github.com>
2026-03-02 14:13:37 +0100

    Update 1_16S_rRNA_pipeline.sh

--- 3_species_delimitation_methods/1_16S_rRNA_gene/1_16S_rRNA_pipeline.sh
+++ 3_species_delimitation_methods/1_16S_rRNA_gene/1_16S_rRNA_pipeline.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+# Author: Khaoula El Mchachti
+# Description: Extract 16S rRNA sequences from Prokka annotations and perform MAFFT alignment
+# Input: Prokka .ffn files
+# Output: 16S_sequences.fasta, 16S_aligned.fasta
+# Date: 2026-03-02
 
-echo "Extracting and aligning 16S sequences"
+echo "===== Extracting and aligning 16S sequences ====="
 
 # Step 1: Extract 16S rRNA genes from Prokka .ffn
-
 echo "Extracting 16S sequences from .ffn files..."
 mkdir -p 16S_seq
+
 for ffn in ~/Bacterial_species_delimitation/2_genomic_analyses/prokka/prokka_results/*/*.ffn; do
     base=$(basename "$ffn" .ffn)
     seqkit grep -nrp "16S.*ribosomal.*RNA" "$ffn" > "16S_seq/${base}.fasta"
@@ -25,7 +30,6 @@ done
 cat 16S_all/*.fasta > 16S_sequences.fasta
 
 # Step 4: Align with MAFFT
-
 echo "Running MAFFT alignment..."
 mafft --auto 16S_sequences.fasta > 16S_aligned.fasta
 
