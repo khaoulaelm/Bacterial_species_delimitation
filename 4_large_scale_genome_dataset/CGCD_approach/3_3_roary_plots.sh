63ec3a575735f3dcc467accca6c8ee08e3121e68
khaoulaelm <136265673+khaoulaelm@users.noreply.github.com>
2026-04-07 18:48:17 +0200

    Create 3_3_roary_plots.sh

---
+++ 4_large_scale_genome_dataset/CGCD_approach/3_3_roary_plots.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Author: Khaoula El Mchachti
+# Description: Plotting Roary results using roary_plots.py
+# Input: tree.newick, gene_presence_absence.csv
+# Output: Roary plot files (plots)
+# Date: 2026-04-03
+
+echo "===== Starting Roary plotting ======"
+
+# Plot Roary results
+echo "Plotting Roary results..."
+python3 roary_plots.py \
+  --labels \
+  roary_results/tree.newick \
+  roary_results/gene_presence_absence.csv
