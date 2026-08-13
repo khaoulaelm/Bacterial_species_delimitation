f26f79a6ffce5b4c2868e6af9783da78e3a5e889
khaoulaelm <136265673+khaoulaelm@users.noreply.github.com>
2026-08-13 13:59:19 +0200

    Update 2_16S_rRNA_abgd_asap.sh

--- 3_species_delimitation_methods/1_16S_rRNA_gene/2_16S_rRNA_abgd_asap.sh
+++ 3_species_delimitation_methods/1_16S_rRNA_gene/2_16S_rRNA_abgd_asap.sh
@@ -4,30 +4,48 @@
 # Input: 16S_aligned.fasta
 # Output: abgd_output/, asap_output/
 # Date: 2026-03-02
+# Last modified: 2026-08-12
+
+# Find the directory
+SCRIPT_DIR="$(cd "$(dirname "$BASH_SOURCE[0]}")" && pwd)"
+
+#Project root directory
+PROJECT_DIR="$(cd "$SCRIPT_DIR/../.." && pwd)"
 
 #ABGD
+ABGD_DIR="$PROJECT_DIR/3_species_delimitation_methods/ABGD/abgd"
+
+#16S alignment
+ALIGNMENT="$PROJECT_DIR/3_species_delimitation_methods/1_16S_rRNA_gene/16S_aligned.fasta"
+
+# ASAP
+ASAP_DIR="$PROJECT_DIR/3_species_delimitation_methods/ASAP/asap"
+
 echo "Running ABGD species delimitation..."
+
+# Make ABGD executable 
+chmod +x "$ABGD_DIR"
 
 mkdir -p abgd_output
 
-~/Bacterial_species_delimitation/3_species_delimitation_methods/ABGD/abgd \
+"$ABGD_DIR" \
   -a \
-  -o abgd_output/ \
+  -o abgd_output \
   -d JC69 \
-  ~/Bacterial_species_delimitation/3_species_delimitation_methods/1_16S_rRNA_gene/16S_aligned.fasta
+  "$ALIGNMENT"
 
 echo "ABGD finished"
 
-
-# ASAP
-
 echo "Running ASAP species delimitation..."
+
+# Make ASAP executable 
+chmod +x "$ASAP_DIR"
 
 mkdir -p asap_output
 
-~/Bacterial_species_delimitation/3_species_delimitation_methods/ASAP/asap \
-  -u\
-  -o asap_output/ \
-  ~/Bacterial_species_delimitation/3_species_delimitation_methods/1_16S_rRNA_gene/16S_aligned.fasta
+"$ASAP_DIR" \
+  -u \
+  -o asap_output \
+  "$ALIGNMENT"
   
 echo "ASAP finished"
