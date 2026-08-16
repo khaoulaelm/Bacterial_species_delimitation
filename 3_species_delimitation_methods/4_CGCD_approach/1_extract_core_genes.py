68d39af8b40746d945cb45c63adc8cfa2678396c
khaoulaelm <136265673+khaoulaelm@users.noreply.github.com>
2026-08-17 01:04:53 +0200

    Update 1_extract_core_genes.py

--- 3_species_delimitation_methods/4_CGCD_approach/1_extract_core_genes.py
+++ 3_species_delimitation_methods/4_CGCD_approach/1_extract_core_genes.py
@@ -6,16 +6,41 @@ Description: Extract all core genes (present in all strains)
 Input: gene_presence_absence.csv, prokka_results/
 Output: /core_genes_fasta/<gene_name>.fasta (one file per core gene; headers renamed to strain)
 Date: 2026-03-20
+Last modified: 2026-08-16
 """
 
 import os
 import pandas as pd
 from Bio import SeqIO
 
+print("===== Extracting core gene sequences =====")
+
+# Find the directory containing this script
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Project root directory
+PROJECT_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, "../.."))
+
 # Paths
-csv_path = os.path.expanduser("~/Bacterial_species_delimitation/2_genomic_analyses/roary/roary_results/gene_presence_absence.csv")
-prokka_base_dir = os.path.expanduser("~/Bacterial_species_delimitation/2_genomic_analyses/prokka/prokka_results")
-output_dir = os.path.expanduser("~/Bacterial_species_delimitation/3_species_delimitation_methods/4_CGCD_approach/core_genes_fasta")
+csv_path = os.path.join(
+    PROJECT_DIR,
+    "2_genomic_analyses",
+    "2_roary",
+    "roary_results",
+    "gene_presence_absence.csv"
+    )
+prokka_base_dir = os.path.join(
+    PROJECT_DIR,
+    "2_genomic_analyses",
+    "1_prokka",
+    "prokka_results"
+    )
+output_dir = os.path.join(
+    PROJECT_DIR,
+    "3_species_delimitation_methods",
+    "4_CGCD_approach",
+    "core_genes_fasta"
+    )
 
 # Make sure output directory exists
 os.makedirs(output_dir, exist_ok=True)
