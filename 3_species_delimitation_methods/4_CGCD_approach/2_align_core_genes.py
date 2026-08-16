ca40854523995a2e9865936d1246965ff1e9d7f4
khaoulaelm <136265673+khaoulaelm@users.noreply.github.com>
2026-08-17 01:06:12 +0200

    Update 2_align_core_genes.py

--- 3_species_delimitation_methods/4_CGCD_approach/2_align_core_genes.py
+++ 3_species_delimitation_methods/4_CGCD_approach/2_align_core_genes.py
@@ -6,18 +6,36 @@ Description: Align all core gene FASTA files using MAFFT
 Input: core_genes_fasta/*.fasta
 Output: core_genes_aligned/*_aligned.fasta
 Date: 2026-03-20
+Last modified: 2026-08-16
 """
 
 import os
 import subprocess
 
+# Find the directory containing this script
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Project root directory
+PROJECT_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, "../.."))
+    
 # Define input/output
-input_dir = os.path.expanduser("~/Bacterial_species_delimitation/3_species_delimitation_methods/4_CGCD_approach/core_genes_fasta")
-output_dir = os.path.expanduser("~/Bacterial_species_delimitation/3_species_delimitation_methods/4_CGCD_approach/core_genes_aligned")
+input_dir = os.path.join(
+    PROJECT_DIR,
+    "3_species_delimitation_methods",
+    "4_CGCD_approach",
+    "core_genes_fasta"
+    )
+output_dir = os.path.join(
+    PROJECT_DIR,        
+    "3_species_delimitation_methods",
+    "4_CGCD_approach",
+    "core_genes_aligned"
+    )
 
 os.makedirs(output_dir, exist_ok=True)
 
 print("===== Starting MAFFT alignments =====")
+print("Please make sure that the appropriate Conda environment is activated. MAFFT is required for this analysis.") 
 
 for fasta_file in os.listdir(input_dir):
     if fasta_file.endswith(".fasta"):
