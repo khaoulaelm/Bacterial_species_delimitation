463bfc28881596ee7d0b381a516ef45e07b3e417
khaoulaelm <136265673+khaoulaelm@users.noreply.github.com>
2026-08-17 20:56:56 +0200

    Update 4_7_asap_extract_groups.py

--- 3_species_delimitation_methods/4_CGCD_approach/4_7_asap_extract_groups.py
+++ 3_species_delimitation_methods/4_CGCD_approach/4_7_asap_extract_groups.py
@@ -5,27 +5,58 @@ Description: Extract strain groups for each threshold within a detected plateau 
 Input: ASAP_conspecificity_matrix.csv
 Output: ASAP_groups_plateau/groups_t{threshold}.csv
 Date: 2026-03-30
+Last modified: 2026-08-17
 """
 
 import pandas as pd
 import networkx as nx
 import os
 
+# Find the directory containing this script
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Project root directory
+PROJECT_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, "../.."))
+
+#Input: ASAP conspecificity matrix
+matrix_file = os.path.join(
+    PROJECT_DIR,
+    "3_species_delimitation_methods",
+    "4_CGCD_approach",
+    "ASAP_conspecificity_matrix",
+    "ASAP_conspecificity_matrix.csv"
+)
+
 # Load conspecificity matrix
-df = pd.read_csv("ASAP_conspecificity_matrix/ASAP_conspecificity_matrix.csv", index_col=0)
+df = pd.read_csv(matrix_file, index_col=0)
 
 # All strains
 strains = df.index.tolist()
 
-# Plateau range (from your plot)
-print("NOTE: Please update START and END to match the plateau range from your plot before running.")
+# Plateau range obtained from the pervious threshold scan.
+# Read the detected plateau
+plateau_file = os.path.join(
+    PROJECT_DIR,
+    "3_species_delimitation_methods",
+    "4_CGCD_approach",
+    "ASAP_plots",
+    "best_plateau.txt"
+)
 
-START = 1555
-END = 2090
-
+with open(plateau_file) as f:
+   START, END = map( int, f.read().strip().split())
+   
 # Output directory
-output_dir = "ASAP_groups_plateau"
+output_dir = os.path.join(
+    PROJECT_DIR,
+    "3_species_delimitation_methods",
+    "4_CGCD_approach",
+    "ASAP_groups_plateau"
+)
+
 os.makedirs(output_dir, exist_ok=True)
+
+print("Extracting groups for thresholds:", START, "to", END)
 
 for threshold in range(START, END + 1):
 
@@ -50,10 +81,15 @@ for threshold in range(START, END + 1):
                 "Group": gid
             })
 
-    pd.DataFrame(rows).to_csv(
-        f"ASAP_groups_plateau/groups_t{threshold}.csv",
-        index=False
+    output_file = os.path.join(
+        output_dir,
+        f"groups_t{threshold}.csv"
     )
 
+    pd.DataFrame(rows).to_csv(
+        output_file,
+        index=False
+    )
+    
 print("Done. Groups extracted for all thresholds in the plateau.")
-print(f"Output directory: {os.path.abspath(output_dir)}")
+print(f"Output directory: {output_dir}")
